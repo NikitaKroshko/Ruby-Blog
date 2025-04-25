83c1d3dd0df119fd314e4865d7431d3b942c9d90
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-04-25 23:12:46 +0200

    Working comments that can be added to any page

--- ruby_blog/app/controllers/comments_controller.rb
+++ ruby_blog/app/controllers/comments_controller.rb
@@ -1,12 +1,20 @@
 class CommentsController < ApplicationController
 
+ 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment]).permit(:name, :comment)
+    @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
 
+
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :comment)
   end
 
 
