c7f2fc056a991692fcc4963fdce5afd478b42f8a
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-04-25 20:26:27 +0200

    Necessary changes to the db

--- ruby_blog/db/schema.rb
+++ ruby_blog/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_25_181839) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_25_182227) do
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
