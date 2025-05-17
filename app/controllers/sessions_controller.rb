74b37d0e96f630395a749c613857507dc70bfd39
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-17 13:04:09 +0100

    Populate the session controlled:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:session][:username])
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      flash.now[:alert] = "Invalid username or password"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path, notice: "Logged out successfully"
   end
 end
+
