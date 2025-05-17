5e0e7282b9588b2a12b83f9cb587116eb90010f4
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-17 13:25:35 +0100

    Alter the routes rb

--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
 
-  get '/login', to: 'sessions#new', as: :login
-  post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy', as: :logout
+  
+  get "/login", to: "sessions#new", as: :new_session
+  post "/login", to: "sessions#create", as: :sessions
+  delete "/logout", to: "sessions#destroy", as: :logout
 
   resources :posts do
     resources :comments
