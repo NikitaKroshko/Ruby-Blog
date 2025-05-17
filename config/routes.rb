738a0f8f83c560b771593b9850443875441f4f8d
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-17 12:45:54 +0100

    Add the users to the routed rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   
+
+  resources :users, only: [:new, :create]
   resources :posts do
     resources :comments
   end
