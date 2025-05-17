a1cb6c75480ae7f8e83a85a3bf0b3819ba9bef1d
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-17 13:24:42 +0100

    Fix the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  helper_method :current_user
+  helper_method :current_user, :user_signed_in?
   allow_browser versions: :modern
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def user_signed_in?
+    current_user.present?
   end
 end
