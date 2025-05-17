8350a8986ad57a655e78a106d36c3e2e81610cfd
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-17 12:55:32 +0100

    Add show to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :posts do
     resources :comments
   end
