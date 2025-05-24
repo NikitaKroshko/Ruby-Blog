337bb31b153d85710656fab256dbb51c43de49d7
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-24 11:04:46 +0100

    Add an application helper for rendering markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
+    options = {
+      autolink: true,
+      tables: true,
+      fenced_code_blocks: true
+    }
+    markdown = Redcarpet::Markdown.new(renderer, options)
+    markdown.render(text).html_safe
+  end
 end
+
