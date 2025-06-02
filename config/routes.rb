915a7c871f7e622a5c9a2f2a19a1c14ec7afc015
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-02 21:21:39 +0100

    Add update to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :users, only: [:new, :create, :show] do
+  resources :users, only: [:new, :create, :show, :update] do
     member do
       get 'setting'
     end
