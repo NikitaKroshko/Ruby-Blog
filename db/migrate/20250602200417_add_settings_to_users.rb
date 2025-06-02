4658fc6e8ee98e4872f98f6d0913cf467824c804
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-02 21:05:19 +0100

    make the default lightmode and ensure this setting exists

--- db/migrate/20250602200417_add_settings_to_users.rb
+++ db/migrate/20250602200417_add_settings_to_users.rb
@@ -1,5 +1,5 @@
 class AddSettingsToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :night_mode, :boolean
+    add_column :users, :night_mode, :boolean, default: false, null:false
   end
 end
