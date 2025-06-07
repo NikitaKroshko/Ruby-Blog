200bbb849e354166ba70ea4a2b27c8196f6a11f8
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-07 12:37:00 +0100

    migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_02_200417) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_07_113604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_02_200417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "night_mode", default: false, null: false
+    t.text "desc"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
