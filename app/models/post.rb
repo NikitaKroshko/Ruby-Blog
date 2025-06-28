cbb7d351ca74650f8116eb562688c7665b98a37b
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-28 11:28:43 +0200

    add attachments as an optional of post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true
   belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
+  has_many :attachments, optional: true
 end
