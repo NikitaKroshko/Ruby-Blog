c60626191b12575a58598028d71407816f84cd87
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-28 11:29:04 +0200

    fix optional to depndent and destory

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true
   belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
-  has_many :attachments, optional: true
+  has_many :attachments, dependent: :destroy
 end
